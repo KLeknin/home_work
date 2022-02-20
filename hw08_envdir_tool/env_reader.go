c9b704d2ae6213803dc1ab9d26b07f7e1898cda5
KLeknin <KLeknin@gmail.com>
2022-02-20 17:30:43 +0300

    Appended check for nil stages

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +16,50 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func fileFirstLine(fileName string) (value string, err error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer func(e *error) {
+		err := file.Close()
+		if *e == nil && err != nil {
+			*e = err
+		}
+	}(&err)
+
+	reader := bufio.NewReader(file)
+	value, err = reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	return
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
+	fi, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var env Environment
+	var envValue EnvValue
+	for _, f := range fi {
+		if f.IsDir() {
+			continue
+		}
+		envValue = EnvValue{}
+		envValue.NeedRemove = f.Size() == 0
+		if !envValue.NeedRemove {
+			envValue.Value, err = fileFirstLine(f.Name())
+			if err != nil {
+				return nil, fmt.Errorf("errror reading file %s: %v", f.Name(), err)
+			}
+		}
+		env[f.Name()] = envValue
+
+	}
 	return nil, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	// Place your code here.
-	return
+	cmdRun := exec.Command(cmd[0], cmd[2:]...)
+	var cmdEnv []string
+	for en := range env {
+		cmdEnv = append(cmdEnv, en+"="+env[en].Value)
+	}
+	cmdRun.Env = cmdEnv
+	cmdRun.Stdin = os.Stdin
+	cmdRun.Stdout = os.Stdout
+
+	if err := cmdRun.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	return cmdRun.ProcessState.ExitCode()
 }
