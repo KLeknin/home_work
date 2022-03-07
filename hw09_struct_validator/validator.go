f0098f688acc4f98be988a82474bdebde0e94937
KLeknin <KLeknin@gmail.com>
2022-03-07 22:12:53 +0300

    hw09_struct_validator

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -3,6 +3,9 @@ package hw09structvalidator
 import (
 	"fmt"
 	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type ValidationError struct {
@@ -16,13 +19,59 @@ func (v ValidationErrors) Error() string {
 	panic("implement me")
 }
 
-func validateString(s, tag string) error {
-	fmt.Printf("%v, %T\n\n", s, s)
-	return nil
+func validateString(s, tags string) (err error, vErr []ValidationError) {
+	vErr = make([]ValidationError, 0, 0)
+	tagKeyValue := make([]string, 0, 2)
+	for _, oneTag := range strings.Split(tags, "|") {
+		if oneTag == "" {
+			continue
+		}
+		tagKeyValue = strings.SplitN(oneTag, ":", 2)
+		if len(tagKeyValue) < 2 || tagKeyValue[1] == "" {
+			return fmt.Errorf("tag key without value:\"%v\"", oneTag), vErr
+		}
+		switch tagKeyValue[0] {
+		case "len": //len:32 - длина строки должна быть ровно 32 символа;
+			{
+				lenExpected, err := strconv.Atoi(tagKeyValue[1])
+				if err != nil {
+					return fmt.Errorf("tag key wrong parametr:\"%v\"", oneTag), vErr
+				}
+				if lenExpected != len(s) {
+					vErr = append(vErr, ValidationError{s, fmt.Errorf("wrong string length, expected:%v", lenExpected)})
+				}
+			}
+		case "regexp": //regexp:\\d+ - согласно регулярному выражению строка должна состоять из цифр (\\ - экранирование слэша);
+			{
+				rExp, err := regexp.Compile(tagKeyValue[1])
+				if err != nil {
+					return fmt.Errorf("tag key dont contain regular expression:\"%v\"", oneTag), vErr
+				}
+				if !rExp.MatchString(s) {
+					vErr = append(vErr, ValidationError{s, fmt.Errorf("string \"%v\" dont match regular tag \"%v\"", s, oneTag)})
+				}
+			}
+		case "in": //in:foo,bar - строка должна входить в множество строк {"foo", "bar"}.
+			{
+				found := false
+				for _, str := range strings.Split(tagKeyValue[1], ",") {
+					if str == s {
+						found = true
+						break
+					}
+				}
+				if !found {
+					vErr = append(vErr, ValidationError{s, fmt.Errorf("string \"%v\" dont in tag list \"%v\"", s, oneTag)})
+				}
+			}
+		}
+	}
+	return nil, vErr
 }
-func validateInt(i int64, tag string) error {
-	fmt.Printf("%v, %T\n\n", i, i)
-	return nil
+func validateInt(i int64, tag string) (err error, vErr ValidationErrors) {
+	vErr = make([]ValidationError, 0, 0)
+	//tagKeyValue := make([]string, 0, 2)
+	return nil, vErr
 }
 
 func Validate(v interface{}) error {
@@ -35,6 +84,8 @@ func Validate(v interface{}) error {
 		return fmt.Errorf("data is not structure: %v", v)
 	}
 
+	validationErrors := make([]ValidationError, 0)
+	vErr := make([]ValidationError, 0)
 	var err error
 	st := reflect.ValueOf(v)
 	for i := 0; i < st.NumField(); i++ {
@@ -52,18 +103,19 @@ func Validate(v interface{}) error {
 					fv := fieldValue.Index(i)
 					switch fv.Kind() {
 					case reflect.String:
-						err = validateString(fv.String(), tagStr)
+						err, vErr = validateString(fv.String(), tagStr)
 					case reflect.Int:
-						err = validateInt(fv.Int(), tagStr)
+						err, vErr = validateInt(fv.Int(), tagStr)
 					}
 					//todo обработать err
+					validationErrors = append(validationErrors, vErr...)
 				}
 			}
 		case reflect.String:
-			err = validateString(fieldValue.String(), tagStr)
+			err, vErr = validateString(fieldValue.String(), tagStr)
 			//todo обработать err
 		case reflect.Int:
-			err = validateInt(fieldValue.Int(), tagStr)
+			err, vErr = validateInt(fieldValue.Int(), tagStr)
 			//todo обработать err
 		}
 	}
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,6 +3,7 @@ package hw09structvalidator
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -59,14 +60,99 @@ func TestValidate(t *testing.T) {
 	}
 }
 
-func TestMe(t *testing.T) {
-	str := struct {
-		sl []string `validate:"in:200,404,500"`
-		s  string   `validate:"in:200,404,500"`
-		i  int      `validate:"in:200,404,500"`
-	}{}
-	str.sl = []string{"a", "b"}
-	str.s = "my string"
-	str.i = 123
-	Validate(str)
+type tstData struct {
+	s, tags string
+	res     []ValidationError
+	err     error
+}
+
+func TestValidateString(t *testing.T) {
+	t.Run("good", func(t *testing.T) {
+		testData := []tstData{
+			{"", "", []ValidationError{}, nil},
+			{"", "любые:левые:тэги", []ValidationError{}, nil},
+			{"Любая строка", "", []ValidationError{}, nil},
+			{"Любая строка", "любые:левые:тэги", []ValidationError{}, nil},
+			//len:32 - длина строки должна быть ровно 32 символа;
+			{"", "len:0", []ValidationError{}, nil},
+			{"1", "len:1", []ValidationError{}, nil},
+			{"12345678901234567890123456789012", "len:32", []ValidationError{}, nil},
+			//regexp:\\d+ - согласно регулярному выражению строка должна состоять из цифр (\\ - экранирование слэша);
+			{"12345678901234567890123456789012", "regexp:\\d+", []ValidationError{}, nil},
+			//in:foo,bar - строка должна входить в множество строк {"foo", "bar"}.
+			{"a", "in:a", []ValidationError{}, nil},
+			{"a", "in:b,a,c", []ValidationError{}, nil},
+			{"a", "in:b,c,a", []ValidationError{}, nil},
+			{"a", "in:a,b,c", []ValidationError{}, nil},
+			{"a", "in:a,a,a", []ValidationError{}, nil},
+			//{"", "in:a,a,a", []ValidationError{}, nil},
+			//{"", "in:", []ValidationError{}, nil},
+			//Допускается комбинация валидаторов по логическому "И" с помощью |, например:
+			{"12345678901234567890123456789012", "len:32|regexp:\\d+", []ValidationError{}, nil},
+			{"123", "len:3|regexp:\\d+|in:a,123,b", []ValidationError{}, nil},
+		}
+		for i, testD := range testData {
+			t.Run(fmt.Sprint(i), func(t *testing.T) {
+				//vErr := make(ValidationErrors, 0)
+				err, vErr := validateString(testD.s, testD.tags)
+				require.Nilf(t, err, "Error: %v", err)
+				require.Equal(t, testD.res, vErr, "Results different.\nExpected:%v\nGained:  %v", testD.res, vErr)
+				if len(vErr) > 0 {
+					println(vErr)
+				}
+			})
+		}
+	})
+	t.Run("badTag", func(t *testing.T) {
+		testData := []tstData{
+			{"a", "len:2",
+				[]ValidationError{{"a",
+					fmt.Errorf("wrong string length, expected:%v", 2),
+				}},
+				nil},
+			{"a", "regexp:\\d+",
+				[]ValidationError{{"a",
+					fmt.Errorf("string \"%v\" dont match regular tag \"%v\"", "a", "regexp:\\d+"),
+				}},
+				nil},
+			{"a", "in:b,c",
+				[]ValidationError{{"a",
+					fmt.Errorf("string \"%v\" dont in tag list \"%v\"", "a", "in:b,c"),
+				}},
+				nil},
+		}
+		for i, testD := range testData {
+			t.Run(fmt.Sprint(i), func(t *testing.T) {
+				//vErr := make(ValidationErrors, 0)
+				err, vErr := validateString(testD.s, testD.tags)
+				require.Nilf(t, err, "Error: %v", err)
+				for j, _ := range vErr {
+					require.Equal(t, testD.res[j].Err.Error(), vErr[j].Err.Error(), "Results different.\nExpected:%v\nGained:  %v", testD.res[j], vErr[j])
+				}
+			})
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		testData := []tstData{
+			{"", "len:", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "len:")},
+			{"", "in:", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "in:")},
+			{"", "regexp:", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "regexp:")},
+			{"", "len", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "len")},
+			{"", "in", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "in")},
+			{"", "regexp", []ValidationError{}, fmt.Errorf("tag key without value:\"%v\"", "regexp")},
+			{"", "regexp:)", []ValidationError{}, fmt.Errorf("tag key dont contain regular expression:\"%v\"", "regexp:)")},
+			{"", "len:0|regexp:)", []ValidationError{}, fmt.Errorf("tag key dont contain regular expression:\"%v\"", "regexp:)")},
+			{"", "len:стопятьсот", []ValidationError{}, fmt.Errorf("tag key wrong parametr:\"%v\"", "len:стопятьсот")},
+		}
+		for i, testD := range testData {
+			t.Run(fmt.Sprint(i), func(t *testing.T) {
+				//vErr := make(ValidationErrors, 0)
+				err, vErr := validateString(testD.s, testD.tags)
+				require.Equal(t, testD.err, err, "No or incorrect error.\nExpected:%v\nGained:  %v", testD.err, err)
+				require.Equal(t, testD.res, vErr, "Results different.\nExpected:%v\nGained:  %v", testD.res, vErr)
+			})
+		}
+	})
+
 }
